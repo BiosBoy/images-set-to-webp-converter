f0b3fa6e34ddf29193994b3334de092bcc8228be
sviatoslav <svyat770@gmail.com>
2020-02-14 15:06:13 +0800

    Test

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
 export const WEBP_EXTENSION = '.webp'
 export const PATH_TO_PARSE = process.argv[process.argv.length - 2]
 export const POLICE_ALARM_ICON = '🚨'
+export const POLICE_ALARM_2ICON = '🚨'
